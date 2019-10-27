40a1a9ba78e0eba79839b20dd96a65f85d078810
BossierCrane <55777802+BossierCrane@users.noreply.github.com>
2019-10-27 16:18:51 +1000

    The bad way

--- select.js
+++ select.js
@@ -1,19 +1,13 @@
-var pic0 = "Choice 1.png";
-var pic1 = "Choice 2P.png";
-var pic2 = "Choice 2H.png";
-var pic3P = "Double Punch ending.png"
-var pic4P = "Forgiveness.png"
-var pic3H = "Eternity Hug.png"
-var pic4H = "Eternal adventure.png"
+
+var pusher = new Pusher('7c0ae57426d90de7c792', {
+  cluster: 'ap4',
+  forceTLS: true
+});
 
 Pusher.logToConsole = true;
 
-    var pusher = new Pusher('7c0ae57426d90de7c792', {
-      cluster: 'ap4',
-      forceTLS: true
-    });
 
-    var channel = pusher.subscribe('my-channel');
-    channel.bind('my-event', function(data) {
-      alert(JSON.stringify(data));
-    });
+var channel = pusher.subscribe('my-channel');
+channel.bind('my-event', function(data) {
+  alert(JSON.stringify(data));
+});
